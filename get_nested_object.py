77a071b5ed55be737dc6873284aa3400bfff6c6f
gvkusuma <103498634+gvkusuma@users.noreply.github.com>
2023-10-02 08:56:14 +0530

    Create get_nested_object.py

---
+++ get_nested_object.py
@@ -0,0 +1,22 @@
+def get_nested_value(obj, key):
+    keys = key.split('/')
+    current_obj = obj
+    for k in keys:
+        if k in current_obj:
+            current_obj = current_obj[k]
+        else:
+            return None
+    return current_obj
+
+# Example usages
+object1 = {"a": {"b": {"c": "d"}}}
+key1 = "a/b/c"
+value1 = get_nested_value(object1, key1)
+print(value1) 
+# Output: "d"
+
+object2 = {"x": {"y": {"z": "a"}}}
+key2 = "x/y/z"
+value2 = get_nested_value(object2, key2)
+print(value2)  
+# Output: "a"
